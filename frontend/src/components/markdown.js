7fcf9d0849202f686c4ed048f9b4ea9c9c5652a3
Guido van Loon <gdh.van.loon@gmail.com>
2020-05-25 10:33:33 +0200

    Added youtube and vimeo support

--- frontend/src/components/markdown.js
+++ frontend/src/components/markdown.js
@@ -3,12 +3,19 @@ import ReactMarkdown from 'react-markdown'
 import breaks from 'remark-breaks'
 import JsxParser from 'react-jsx-parser'
 import {template} from 'common'
+import Youtube from './media/youtube'
+import Vimeo from './media/vimeo'
 
 export default ({ source, strings, components}) =>  {
     source = template(source, strings)
-    const transforms = Object.assign({
-        React: (props) => <>{props.children}</>
-    }, components)
+    const transforms = Object.assign(
+    {
+            React: (props) => <>{props.children}</>,
+            Youtube: (props) => <Youtube {...props} />,
+            Vimeo: (props) => <Vimeo {...props} />
+        },
+        components
+    )
     const renderers = {
         html: (props) => <JsxParser jsx={props.value} components={transforms} />
     }
