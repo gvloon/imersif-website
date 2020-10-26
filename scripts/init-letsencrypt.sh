706b0da7021ecbf74ba210ca24152bbce410a177
Guido van Loon <gdh.van.loon@gmail.com>
2020-10-26 11:58:57 +0000

    Production update

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -10,7 +10,7 @@ domains=(xr-patterns.com www.xr-patterns.com)
 rsa_key_size=4096
 data_path="certbot"
 email="developer@imersif.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
